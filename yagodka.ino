bf2e0821016cf01ae7d56f59e940440ea157ea99
Mike Schekotov <mschekotov@cs-cart.com>
2018-10-24 23:06:57 +0400

    Media events added.

--- yagodka.ino
+++ yagodka.ino
@@ -1,21 +1,29 @@
+// Sketch > Include library > HID-Project (https://github.com/NicoHood/HID)
+#include <HID-Project.h>
+#include <HID-Settings.h>
 #include <Bounce2.h>
-#include <Keyboard.h>
 
-#define PIN_RIGHT 11
-#define PIN_SPACE 12
-#define PIN_LEFT  13
+#define PIN_PREV 12
+#define PIN_PLAY 10
+#define PIN_NEXT 8
 
-#define KEY_SPACE 32
+#define ACTION_PREV 1
+#define ACTION_PLAY 2
+#define ACTION_NEXT 3
 
-Bounce left  = Bounce(); 
-Bounce space = Bounce();
-Bounce right = Bounce();
+Bounce prev = Bounce(); 
+Bounce play = Bounce();
+Bounce next = Bounce();
 
-bool s_left  = false;
-bool s_space = false;
-bool s_right = false;
+bool s_prev = false;
+bool s_play = false;
+bool s_next = false;
 
-void attachBtnToPin(Bounce &btn, int pin, int interval = 2)
+// false: Arrow left, Arrow right and Space button events are triggered
+// true:  Previous, Next and Play/Pause media events are triggered
+bool is_media = true;
+
+void attachBtnToPin(Bounce &btn, int pin, int interval = 500)
 {
   pinMode(pin, INPUT_PULLUP);
   btn.attach(pin);
@@ -29,26 +37,49 @@ bool readBtn(Bounce &btn)
   return !btn.read();
 }
 
-void simulateKey(Bounce &btn, char key, bool &btn_state)
+void simulateKey(Bounce &btn, byte action, bool &btn_state)
 {
-  if (readBtn(btn) != btn_state) {
-    Keyboard.write(key);
+  if (readBtn(btn) != btn_state) {    
+    if (action == ACTION_PREV) {
+      if (is_media) {
+        Consumer.write(MEDIA_PREVIOUS);
+      } else {
+        Keyboard.write(KEY_LEFT_ARROW);
+      }
+    } else if (action == ACTION_NEXT) {
+        if (is_media) {
+          Consumer.write(MEDIA_NEXT);
+        } else {
+          Keyboard.write(KEY_RIGHT_ARROW);
+        }
+    } else {
+        if (is_media) {
+          Consumer.write(MEDIA_PLAY_PAUSE);
+        } else {
+          Keyboard.write(' ');
+        }
+    }
+    
     btn_state = !btn_state;
   }
 }
 
 void setup()
 {
-  attachBtnToPin(left,  PIN_LEFT);
-  attachBtnToPin(right, PIN_RIGHT);
-  attachBtnToPin(space, PIN_SPACE);
+  attachBtnToPin(prev, PIN_PREV);
+  attachBtnToPin(next, PIN_NEXT);
+  attachBtnToPin(play, PIN_PLAY);
 
-  Keyboard.begin();
+  if (is_media) {
+    Consumer.begin();
+  } else {
+    Keyboard.begin();
+  }
 }
 
 void loop()
 {
-  simulateKey(left,  KEY_LEFT_ARROW,  s_left);
-  simulateKey(right, KEY_RIGHT_ARROW, s_right);
-  simulateKey(space, KEY_SPACE,       s_space);
+  simulateKey(prev, ACTION_PREV, s_prev);
+  simulateKey(next, ACTION_NEXT, s_next);
+  simulateKey(play, ACTION_PLAY, s_play);
 }
